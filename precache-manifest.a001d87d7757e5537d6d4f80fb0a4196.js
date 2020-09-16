fa49679c1f73b9e104425fee0abf84bb91684320
PHLoc <phunghloc@gmail.com>
2020-09-16 11:11:29 +0700

    Updates

---
+++ precache-manifest.a001d87d7757e5537d6d4f80fb0a4196.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51312b37b72e1de3b5ef1a02b4c9749d",
+    "url": "/burgerbuilder/index.html"
+  },
+  {
+    "revision": "050eb8962d55ab67eef7",
+    "url": "/burgerbuilder/static/css/main.9ac08626.chunk.css"
+  },
+  {
+    "revision": "46a43e3202920dd13f90",
+    "url": "/burgerbuilder/static/js/2.b2bcd984.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/burgerbuilder/static/js/2.b2bcd984.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "050eb8962d55ab67eef7",
+    "url": "/burgerbuilder/static/js/main.14ef0924.chunk.js"
+  },
+  {
+    "revision": "0dd5acb13e01fea9c81f",
+    "url": "/burgerbuilder/static/js/runtime-main.8e44f8fe.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burgerbuilder/static/media/burger-logo.b8503d26.png"
+  }
+]);
